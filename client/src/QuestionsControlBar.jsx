9080883083bb149e7ac2dd7cb1af99ee1c20bdcc
rickboot <rickallen@gmail.com>
2020-09-28 16:19:19 -0700

    Added webpack and top-level React components

---
+++ client/src/QuestionsControlBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function QuestionsControlBar () {
+  return (
+    <div>
+      <h2>Questions Control Bar</h2>
+    </div>
+  )
+};
+
+export default QuestionsControlBar;
