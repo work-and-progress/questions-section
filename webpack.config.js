4f2b21f4ffd63358da91e52f8a7b8b0c5ff12243
rickboot <rickallen@gmail.com>
2020-09-30 11:58:34 -0700

    Added some temporary inline style to test layout

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,25 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-          },
         },
       },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      // {
+      //   test: /\.css$/,
+      //   loader: "style-loader!css-loader?modules=true"
+      // }
     ],
   },
   output: {
