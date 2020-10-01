8da896262cfe0845c675588706ac0764cad3ab91
rickboot <rickallen@gmail.com>
2020-09-30 22:01:48 -0700

    Rebuild post Git tragedy

--- client/src/components/Questions/QuestionsList/QuestionsList.jsx
+++ client/src/components/Questions/QuestionsList/QuestionsList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Question from './Question/Question';
+import style from './QuestionsList.css';
 
 function QuestionsList () {
   return (
     <div>
-      <h2>QuestionsList</h2>
       <Question />
     </div>
   )
