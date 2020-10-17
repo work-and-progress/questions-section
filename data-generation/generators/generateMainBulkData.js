5de0f06b1b11a6b788fd81710236fa788796d38d
Karin Aizawa <kaizawa77@gmail.com>
2020-10-17 13:00:48 -0700

    data generation script done

--- data-generation/generators/generateMainBulkData.js
+++ data-generation/generators/generateMainBulkData.js
@@ -8,7 +8,7 @@ const writeAnswers = fs.createWriteStream('../generatedData/products-questions-a
 writeAnswers.write('product_id,question_id,question_text,question_date,answer_id,answer_text,answer_date,answer_helpful_yes,answer_helpful_no\n', 'utf8');
 
 function writeLotsOfAnswers(writer, encoding, callback) {
-  let i = 10; // how many units do you want? End goal is 10 mill
+  let i = 10; // amount of total products - 10,000,000 is end goal
   let product_id = 0;
   let totalCounter = 0; // starts with whatever you set i
 
@@ -17,12 +17,13 @@ function writeLotsOfAnswers(writer, encoding, callback) {
     do {
       i -= 1;
       product_id += 1;
-
-      const question_max = 10; // 10 questions maximum per product
-      const question_min = 0;
+      // console.log('product_id', product_id);
 
       const answer_max = 3; // 3 answers maximum per question
       const answer_min = 0;
+
+      const question_max = 10; // 10 questions maximum per product
+      const question_min = 0;
 
       let data = '';
       let question_id = '';
@@ -40,9 +41,12 @@ function writeLotsOfAnswers(writer, encoding, callback) {
       let answer_helpful_no = 0;
 
       const randomNumberOfQuestions = Math.floor(Math.random() * (question_max - question_min + 1) + question_min);
+      if (randomNumberOfQuestions === 0) {
+        data = `${product_id}\n`;
+        writer.write(data, encoding, callback);
+      }
 
       for (let j = 0; j < randomNumberOfQuestions; j += 1) {
-        // question_id = faker.random.uuid(); // 1
         question_id = `${product_id}-${j + 1}`;
         question_text = (faker.lorem.sentence()).slice(0, -1); // 2
         question_text += '?';
@@ -50,11 +54,12 @@ function writeLotsOfAnswers(writer, encoding, callback) {
         question_date = generateRandomDate.generateRandomDateWithinLastThreeMonths();
 
         const randomNumberOfAnswers = Math.floor(Math.random() * (answer_max - answer_min + 1) + answer_min);
-        // console.log('randomNumberOfAnswers: ', randomNumberOfAnswers)
-        totalCounter += randomNumberOfAnswers;
+        if (randomNumberOfAnswers === 0) {
+          data = `${product_id},${question_id},${question_text},${question_date}\n`;
+          writer.write(data, encoding, callback);
+        }
 
         for (let k = 0; k < randomNumberOfAnswers; k += 1) {
-          // question_id = faker.random.uuid(); // 1
           answer_id = `${question_id}-${k + 1}`;
           answer_text = (faker.lorem.sentence()).slice(0, -1); // 2
           answer_text += '.';
@@ -72,9 +77,8 @@ function writeLotsOfAnswers(writer, encoding, callback) {
             ok = writer.write(data, encoding);
           }
         }
+        totalCounter += randomNumberOfAnswers;
       }
-
-      // console.log('Total number of lines written: ', totalCounter); // also works
     } while (i > 0 && ok);
 
     // When the highWaterMark is reached, the write method of createWriteStream will start returning false.
