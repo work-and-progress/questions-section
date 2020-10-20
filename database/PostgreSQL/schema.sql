0d0a52f1a4b7863aae620a4a122638017d1d2937
Karin Aizawa <kaizawa77@gmail.com>
2020-10-20 15:39:34 -0700

    deleting duplicate works i tihnk but just makign sure it does by going back to string '1-1-2' instead of UUID so its easier to confirm

--- database/PostgreSQL/schema.sql
+++ database/PostgreSQL/schema.sql
@@ -3,12 +3,12 @@ psql -h localhost
 DROP DATABASE IF EXISTS unzwilling;
 CREATE DATABASE unzwilling;
 \c unzwilling; -- to switch into unzwilling from karinaizawa, not USE unzwilling;
-
+\dt -- to show tables
 
 DROP TABLE IF EXISTS questions;
 
 CREATE TABLE if not exists questions (
-  question_id uuid PRIMARY KEY, --uuid
+  question_id varchar PRIMARY KEY, --uuid
   product_id SERIAL,
   question_text varchar (150),
   question_date DATE,
@@ -19,8 +19,9 @@ CREATE TABLE if not exists questions (
 )
 
 CREATE TABLE if not exists answers (
-  product_id SERIAL PRIMARY KEY,
-  answer_id uuid, --uuid
+  answer_id varchar PRIMARY KEY, --uuid
+  product_id SERIAL,
+  question_id uuid,
   answer_text varchar (150),
   answer_date DATE,
   answer_user_id uuid, --uuid
@@ -29,7 +30,6 @@ CREATE TABLE if not exists answers (
   answer_user_location varchar (150),
   answer_helpful_yes integer,
   answer_helpful_no integer
-  -- PRIMARY KEY (answer_id)
 )
 
 ------------------------------------------------------------------------
@@ -38,7 +38,8 @@ CREATE TABLE if not exists answers (
 
 DROP TABLE IF EXISTS temporary_table;
 
-create temporary table temporary_table (
+--create temporary table temporary_table (
+create table temporary_table (
   answer_id uuid PRIMARY KEY, --uuid
   product_id SERIAL,
   question_id uuid, --uuid
@@ -64,23 +65,22 @@ from '/Users/karinaizawa/Desktop/questions-section/data-generation/generatedData
 DELIMITER ','
 CSV HEADER;
 
--- need to delete the duplicate questions
-DELETE FROM temporary_table USING (
-      SELECT MIN(ctid) as ctid, key
-        FROM temporary_table
-        GROUP BY key HAVING COUNT(*) > 1
-      ) b
-      WHERE a.key = b.key
-      AND a.ctid <> b.ctid
+-- need to delete the duplicate questions .. not sure if this will work
+-- https://stackoverflow.com/a/12963112/14330883 this is the one that is displayed
+-- https://stackoverflow.com/a/46775289/14330883
 
-
+DELETE FROM temporary_table a USING (
+  SELECT MIN(ctid) as ctid, question_id
+    FROM temporary_table
+    GROUP BY question_id HAVING COUNT(*) > 1
+) b
+WHERE a.question_id = b.question_id
+AND a.ctid <> b.ctid
 
 -- insert into questions table
 insert into questions(product_id,question_id,question_text,question_date,question_user_id,question_user_email,question_username,question_user_location)
 select product_id,question_id,question_text,question_date,question_user_id,question_user_email,question_username,question_user_location
 from temporary_table
-
-
 
 
 -- example
