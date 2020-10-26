46191d34f50f869bc7767ceac330429626ad7020
Karin Aizawa <kaizawa77@gmail.com>
2020-10-26 12:43:06 -0700

    gitignore

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
-# DBMS Benchmarking
-- 37,495,494 total rows imported into Cassandra (around 1 hr 15 min)
-- 37,500,494 total rows imported into PostgresSQL (not sure why this number is higher)
-  - 37,500,494 rows in answers table
-  - 24,999,590 rows in questions table
+# Deployment
 
-## Cassandra (winner)
-- around 2-10 ms typically
-<img src="design-and-docs/cassandraquery1.png">
+1. proxy - just my project
+  - run on port 8083 from this project and see if the API will work from within the proxy project
 
-- highest number seen is 61 ms
-<img src="design-and-docs/cassandraquery2.png">
-
-## PostgreSQL
-
-- join operation takes around 70-90 ms
-<img src="design-and-docs/postgresquery.png">
-
-- not too great
-<img src="design-and-docs/postgresql2.png">
--- k6-stress-test.js
+++ k6-stress-test.js
@@ -5,20 +5,20 @@ import { sleep } from 'k6';
 // Scale the number of request per second: 1, 10, 100, 1K
 export const options = {
   stages: [
-    // { duration: '1s', target: 1 }, // target is number of virtual users, duration is how many per minute
+    // { duration: '1s', target: 1 },
     // { duration: '5m', target: 1 },
-    // { duration: '2m', target: 10 },
-    { duration: '5m', target: 10 },
-    { duration: '2m', target: 100 },
-    { duration: '5m', target: 100 },
-    { duration: '2m', target: 1000 },
-    { duration: '5m', target: 1000 },
+    { duration: '1m', target: 10 },
+    // { duration: '5m', target: 10 },
+    // { duration: '2m', target: 100 },
+    // { duration: '5m', target: 100 },
+    // { duration: '2m', target: 1000 },
+    // { duration: '5m', target: 1000 },
   ],
 };
 export default function () {
   const url = 'http://localhost:3003/questions';
-  let headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
-  let data = {
+  const headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
+  const data = {
     product_id: 7,
     question_text: 'Stress Test 10:00AM Oct 26 2020',
     question_date: '2020-10-22',
@@ -26,50 +26,13 @@ export default function () {
     question_username: 'karebae',
     question_user_location: 'San Francisco, CA',
   };
-  let res = http.post(url, data, { headers: headers });
-  headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
-  res = http.post(url, data, { headers: headers });
+  http.post(url, data, { headers });
 
-  // let responses = http.post({
-  //   method: 'POST',
-  //   url: 'http://localhost:3003/questions',
-  //   body: {
-  //     product_id: 7,
-  //     question_text: 'Stress Test 10:00AM Oct 26 2020',
-  //     question_date: '2020-10-22',
-  //     question_user_email: 'karebae@gmail.com',
-  //     question_username: 'karebae',
-  //     question_user_location: 'San Francisco, CA',
-  //   },
-  //   params: {
-  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //   },
-  // });
-  // let req1 = {
-  //   method: 'GET',
-  //   url: 'http://localhost:3003/questionsAndAnswers/9999999',
-  // };
-  // let req2 = {
-  //   method: 'GET',
-  //   url: 'http://localhost:3003/questionsAndAnswers/91',
-  // };
-  // let req3 = {
-  //   method: 'POST',
-  //   url: 'http://localhost:3003/questions',
-  //   body: {
-  //     product_id: 7,
-  //     question_text: 'Stress Test 10:00AM Oct 26 2020',
-  //     question_date: '2020-10-22',
-  //     question_user_email: 'karebae@gmail.com',
-  //     question_username: 'karebae',
-  //     question_user_location: 'San Francisco, CA',
-  //   },
-  //   params: {
-  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //   },
-  // };
-  // let req4 = {
-  //   method: 'POST',
+  // headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
+  // res = http.post(url, data, { headers: headers });
+
+  // let responses = http.get('http://localhost:3003/questionsAndAnswers/9999999');
+
   //   url: 'http://localhost:3003/answers',
   //   body: {
   //     product_id: 5,
@@ -84,10 +47,6 @@ export default function () {
   //   },
   //   params: {
   //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //   },
-  // };
-  // let responses = http.batch([req3]);
 
-  // https://k6.io/docs/javascript-api/k6-http/batch-requests
   sleep(1);
 }
